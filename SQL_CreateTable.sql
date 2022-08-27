a9b75d6e159bbe265307fa2cc73f71cea8072b34
MHAzusa <ayase7a@gmail.com>
2022-08-27 17:33:46 +0900

    Add playersのテーブル作成

---
+++ SQL_CreateTable.sql
@@ -0,0 +1,10 @@
+create table playeres(
+    player_id serial
+    iidx_id numeric(8,0)
+    email varchar(319)
+    name varchar(6)
+    url text
+    encrypt_password char(64)
+    salt char(32)
+    primary key(player_id)
+);
